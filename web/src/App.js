24258ba07ff00b19b8e786e1d42a737055a95f41
Diego Cadena <35208878+dcalmeida13@users.noreply.github.com>
2020-01-15 23:09:23 -0300

    Add files via upload

--- web/src/App.js
+++ web/src/App.js
@@ -1,44 +1,43 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(()=>{
+    async function loadDevs(){
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  },[])
+
+  async function handleAddDev(data){
+    const response = await api.post('/devs', data)
+
+
+    setDevs([...devs,response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="username_github" required></input>
-          </div>
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required></input>
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Longitude</label>
-              <input name="latitude" id="latitude" required></input>
-            </div>
-            <div className="input-block">
-              <label htmlFor="longitude">Latitude</label>
-              <input name="longitude" id="longitude" required></input>
-            </div>
-          </div>
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/35208878?s=460&v=4" alt="Diego Cadena"/>
-            </header>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
         </ul>
       </main>
     </div>
