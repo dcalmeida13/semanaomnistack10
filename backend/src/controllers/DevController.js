0216dc21148d79bf8a8f895553264dc4319fbed2
Diego Cadena <35208878+dcalmeida13@users.noreply.github.com>
2020-01-18 14:30:06 -0300

    Add files via upload

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require("../utils/parseStringAsArray");
+const {findConnections, sendMessage} = require('../websocket');
 
 module.exports = {
 
@@ -9,8 +10,6 @@ module.exports = {
 
     return response.json(devs);
   },
-
-
 
   async store(request,response) {
       const {github_username, techs, latitude, longitude} = request.body;
@@ -37,6 +36,16 @@ module.exports = {
             techs: techsArray,
             location
           })
+
+          //Filtrar as conexões que estão há no máximo 10km de distância
+          //e que o novo dev tenha pelo menos uma das tecnologias filtradas
+
+          const sendSocketMessageTo = findConnections(
+            {latitude, longitude},
+            techsArray
+          )
+
+          sendMessage(sendSocketMessageTo,'new-dev',dev)
       }
 
     return response.json(dev);
